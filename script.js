407b373e786d5a35f2c619d1279790443bf19e5a
thanapol <thanapol.b@i-iptv.com>
2023-07-21 19:14:28 +0700

    update code

--- script.js
+++ script.js
@@ -5,6 +5,20 @@ let currentWordIndex = 0;
 let currentLetterIndex = 0;
 let delay = 150;
 
+// function 
+addEventListener("scroll", (event) => {
+  lastKnownScrollPosition = window.scrollY;
+  if(lastKnownScrollPosition > 0){
+    document.getElementsByClassName("horizontal")[0].style.display = "none";
+    document.getElementsByClassName("toggle-menu")[0].style.display = "";
+
+    // hide li menu show button togle menu
+  }else{
+    document.getElementsByClassName("horizontal")[0].style.display = "";
+    document.getElementsByClassName("toggle-menu")[0].style.display = "none";
+  }
+});
+
 function spellOutText() {
   if (currentWordIndex >= words.length) {
     return;
